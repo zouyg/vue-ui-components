4c465f1276883e989f3f7a9bfd7fa92d2476ed6f
zouyg <zouyiguo@outlook.com>
2018-09-19 08:52:05 +0800

    让 input 支持 v-model 双向绑定

--- src/input.vue
+++ src/input.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="wrapper" :class="{error}">
         <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
-               @change="$emit('change',$event)"
-               @focus="$emit('focus',$event)"
-               @blur="$emit('blur',$event)"
-               @input="$emit('input',$event)">
+               @change="$emit('change',$event.target.value)"
+               @focus="$emit('focus',$event.target.value)"
+               @blur="$emit('blur',$event.target.value)"
+               @input="$emit('input',$event.target.value)">
         <template v-if="error">
             <icon name="error" class="icon-error"></icon>
             <span class="errorMessage">{{error}}</span>
