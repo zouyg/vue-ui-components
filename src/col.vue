5cbca1a155fbd3af9fedf84c7fcdfc133a25af2a
zouyg <zouyiguo@outlook.com>
2019-01-02 23:02:20 +0800

    优化了一下 row 和 col 组件的代码

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2+'px', paddingRight:gutter/2+'px'}">
+    <div class="col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 </template>
@@ -19,6 +18,21 @@
         data(){
             return {
                 gutter: 0
+            }
+        },
+        computed: {
+            colClass(){
+                let {span, offset} = this;
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
+            },
+            colStyle() {
+                return {
+                    paddingLeft: this.gutter / 2 + 'px',
+                    paddingRight: this.gutter / 2 + 'px'
+                }
             }
         }
     }
